7f0362d3dac03b67b4a0e65a15b28f67f4c5df42
jilin.jin <jilin.jin@ge.com>
2019-09-25 21:46:54 +0800

    update

---
+++ src/routes/notfound/shutdowntime/index.js
@@ -0,0 +1,184 @@
+import React, { useRef, useEffect, useState } from 'react';
+import EasyDrawing from 'easy-drawing';
+import { convertToNum, convertToTime, colors } from './tool';
+
+
+function ShutdownTime(props) {
+    const container = useRef(null);
+    const [option, setOption] = useState({});
+    const lon1 = 116.172003;
+    const lon2 = 116.726761;
+    const lat1 = 39.999001;
+
+    useEffect(() => {
+        const styleDom = window.getComputedStyle(container.current);
+        const width = (window.parseFloat(styleDom.getPropertyValue('width')));
+        const height = (window.parseFloat(styleDom.getPropertyValue('height')));
+        const ratio = height / width;
+        const span = (lon2 - lon1) * ratio;
+        const lat2 = lat1 - span;
+        const polygon = [
+            [lon1, lat1],
+            [lon2, lat1],
+            [lon2, lat2],
+            [lon1, lat2]
+        ]
+
+        const texts = [];
+        const scatters = [];
+        const paths = [];
+
+
+        const axisb = 1.001;
+        const axist = 0.9996;
+        const originPoint = [lon1, lat2 * axisb];
+        const xTop = [lon2, lat2 * axisb];
+        const yTop = [lon1, lat1 * axist];
+
+        //xarrow
+        scatters.push({
+            point: xTop,
+            path: {
+                scale: 0.04,
+                color: 'rgb(100,100,100)',
+                d: "M786.0224 469.4016a49.7664 49.7664 0 0 1 0 85.1968L289.1776 852.5824a49.5616 49.5616 0 0 1-75.1616-42.5984v-595.968a49.5616 49.5616 0 0 1 75.1616-42.5984l496.8448 297.984z"
+            }
+        })
+
+        //yarrow
+        scatters.push({
+            point: yTop,
+            path: {
+                scale: 0.04,
+                rotate: -90,
+                color: 'rgb(100,100,100)',
+                d: "M786.0224 469.4016a49.7664 49.7664 0 0 1 0 85.1968L289.1776 852.5824a49.5616 49.5616 0 0 1-75.1616-42.5984v-595.968a49.5616 49.5616 0 0 1 75.1616-42.5984l496.8448 297.984z"
+            }
+        })
+
+        //xaxis
+        paths.push({
+            points: [
+                originPoint,
+                xTop
+            ],
+            width: 3,
+            color: 'rgb(100,100,100)',
+            animation: 'normal',
+        })
+
+        //yaxis
+        paths.push({
+            points: [
+                originPoint,
+                yTop
+            ],
+            width: 3,
+            color: 'rgb(100,100,100)',
+            animation: 'normal',
+        })
+
+        //init 
+        const areas = [{
+            polygon,
+            fillStyle: 'transparent',
+            strokeStyle: 'transparent'
+        }]
+
+        const xAxis = {
+            type: 'grouping',
+            data: [
+                'CT1',
+                'CT2',
+                'CT3',
+                'CT4',
+                'CT5'
+            ]
+        };
+
+        const yAxis = {
+            type: 'time' //or catagory
+        }
+
+        const data = [
+            ['12:24:30', '18:23:12', '20:34:59', '16:01:20', '21:13:13', '22:45:43', '17:35:45'],
+            ['12:24:30', '18:23:12', '20:34:59', '16:01:20', '21:13:13', '22:45:43', '17:35:45'],
+            ['12:24:30', '18:23:12', '17:34:59', '16:01:20', '21:13:13', '22:45:43', '17:35:45'],
+            ['12:24:30', '18:23:12', '20:34:59', '16:01:20', '21:13:13', '22:45:43', '17:35:45'],
+            ['12:24:30', '15:23:12', '20:34:59', '16:01:20', '21:13:13', '22:45:43', '17:35:45']
+        ]
+
+        let xspan = 0;
+
+        //render legend and xAxis
+        if (xAxis.type === 'grouping') {
+            const xdata = xAxis.data;
+            if (!Array.isArray(xdata)) return;
+            xspan = (lon2 - lon1) / xdata.length;
+            xdata.forEach((it, index) => {
+                const x = (lon1 + index * xspan + lon1 + (index + 1) * xspan) / 2;
+                const y = lat1 * axist;
+                const y2 = lat2 * 1.0004
+                texts.push({
+                    point: [x * 1.0003, y],
+                    size: '1em',
+                    text: it,
+                    color: '#fff'
+                });
+                texts.push({
+                    point: [x, y2],
+                    size: '1em',
+                    text: it,
+                    color: '#fff'
+                });
+                scatters.push({
+                    point: [x, y],
+                    color: colors[index],
+                    size: props.fontSize * 0.4,
+                    mode: 'static'
+                })
+            })
+        }
+
+        let maxValue = null;
+        let minValue = null;
+        const vbottom = 1.002 * lat2;
+        const vtop = 0.998 * lat1;
+        if(yAxis .type === 'time' && xAxis.type === 'grouping'){
+            data.forEach(it => {
+                it.forEach(item => {
+                    const time = convertToNum(item);
+                    maxValue === null ? (maxValue = time) : (maxValue = Math.max(maxValue,time));
+                    minValue === null ? (minValue = time) : (minValue = Math.min(minValue,time));
+                })
+            });
+            data.forEach((it,index) => {
+                const lonspan = xspan / it.length;
+                it.forEach((item,itemIndex) => {
+                    const time = convertToNum(item);
+                    const span = time - minValue;
+                    const percent = span / (maxValue - minValue);
+                    const lat = ( vtop -  vbottom) * percent + vbottom;
+                    const lon = lonspan * (itemIndex + 1) + lon1 + xspan * index;
+                    scatters.push({
+                        point: [lon,lat],
+                        color: colors[index],
+                        size: props.fontSize * 0.25,
+                        mode: 'static'
+                    })
+                })
+            });
+
+        }
+
+
+        setOption({ areas, texts, scatters, paths })
+
+    }, [props.fontSize])
+    return <div ref={container} style={{ width: '100%', height: '100%', position: 'relative' }}>
+        <EasyDrawing  areas={option.areas} texts={option.texts} scatters={option.scatters} paths={option.paths} />
+    </div>
+
+}
+
+export default ShutdownTime;
---
+++ src/routes/notfound/shutdowntime/tool.js
@@ -0,0 +1,35 @@
+export const convertToNum = function (raw) {
+    var num = Number(raw);
+    var result = NaN;
+    if (isNaN(num)) {
+        num = raw.toString();
+        if (num.indexOf(':') !== -1) {
+            var time = num.split(':');
+            var hour = Number(time[0]);
+            var min = Number(time[1]);
+            var sec = Number(time[2]);
+            if (typeof hour === 'number' && typeof min === 'number' && typeof sec === 'number') {
+                result = hour * 3600 + min * 60 + sec;
+            }
+        }
+        return result;
+
+    } else {
+        return num;
+    }
+}
+
+export const convertToTime = function (num) {
+    var result = num;
+    if (typeof num === 'number') {
+        var hour = parseInt(num / 3600).toString().length === 1 ? '0' + parseInt(num / 3600).toString() : parseInt(num / 3600).toString();
+        var minute = num % 3600;
+        var min = parseInt(minute / 60).toString().length === 1 ? '0' + parseInt(minute / 60).toString() : parseInt(minute / 60).toString();
+        var sec = (minute % 60).toString().length === 1 ? '0' + (minute % 60).toString() : (minute % 60).toString();
+        result = hour + ':' + min + ':' + sec;
+
+    }
+    return result;
+}
+
+export const colors = ['#6D95FF', '#FF8888', '#FCBA71', '#52E1CB', '#966FF2', '#DD4B39', '#ED9E9E', '#EBBCB7', '#F48E4A', '#FFB652', '#F7DC00', '#C6DD84', '#4ECB73', '#76DAC4', '#4B9FA0', '#78D0DA', '#00ADD7', '#5090F4', '#425087', '#6766B3', '#965EE3', '#DD7DC1', '#ADACDC', '#CACACA', '#878B8C']

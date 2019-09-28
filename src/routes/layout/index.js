6406755abcf929cd3f461b7dd817f350fada9d11
jilin.jin <jilin.jin@ge.com>
2019-09-28 13:05:10 +0800

    update

--- src/routes/layout/index.js
+++ src/routes/layout/index.js
@@ -1,17 +1,21 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 import { useModel } from 'dva-react-hook';
 import Menu from './menu'
 import Style from './index.less'
 
 function Layout(props) {
     const [{ value: menuShink }, setMenuShink] = useModel('menuShink');
+    const [ { value: history } ] = useModel('history')
     useEffect(() => {
         const width = window.innerWidth;
         if (width < 1200) setMenuShink(true)
     }, [])
+    const homePage = () => {
+        window.location.href = ('https://github.com/jinjilynn/easy-drawing')
+    }
     return <div className={Style['layout-container']}>
         <div className={Style['header']}>
-            <i className="ge-iconfont ge-diqiu" />
+            <i onClick={homePage} className="ge-iconfont ge-diqiu" />
             <span>Easy Drawing</span>
         </div>
         <div className={Style['content-container']}>

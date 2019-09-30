4eeda71b987b956f443fd55291eea5ae1b1b27ed
jilin.jin <jilin.jin@ge.com>
2019-09-30 09:29:48 +0800

    update

--- src/routes/notfound/index.js
+++ src/routes/notfound/index.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import EasyDrawing from 'easy-drawing';
+import { useModel } from 'dva-react-hook';
 import Chart from './shutdowntime';
 import { colors } from './shutdowntime/tool';
 import {
@@ -37,21 +38,22 @@ import {
 import world from './world';
 import style from './index.less'
 function NotFound() {
-    const dom = React.useRef(null)
+    const dom = React.useRef(null);
+    const [{ value: lang }] = useModel('lang');
     const [size, setSize] = React.useState(0);
     const worldAreas = [];
     world.features.forEach(it => {
-        if(it.geometry.type === 'Polygon'){
+        if (it.geometry.type === 'Polygon') {
             worldAreas.push({
-                polygon:it.geometry.coordinates[0],
-                fillStyle:colors[Math.floor(Math.random() * 25)]
+                polygon: it.geometry.coordinates[0],
+                fillStyle: colors[Math.floor(Math.random() * 25)]
             })
-    
-        }else if(it.geometry.type === 'MultiPolygon'){
+
+        } else if (it.geometry.type === 'MultiPolygon') {
             it.geometry.coordinates.forEach(item => {
                 worldAreas.push({
-                    polygon:item[0],
-                    fillStyle:colors[Math.floor(Math.random() * 25)]
+                    polygon: item[0],
+                    fillStyle: colors[Math.floor(Math.random() * 25)]
                 })
             })
         }
@@ -64,27 +66,27 @@ function NotFound() {
     })
     return <div className={style['not-found']}>
         <div className={style['world-map']}>
-            <div className={style['map-title']}>you can use 'easy-drawing' to draw a world map</div>
+            <div className={style['map-title']}>{lang === 'en' ? 'you can use "easy-drawing" to draw a world map' : '你可以用easy-drawing画一张世界地图'}</div>
             <div className={style['map-content']}>
-                <EasyDrawing 
-                    size="cover" 
+                <EasyDrawing
+                    size="cover"
                     areas={worldAreas}
                     paths={
                         [
                             {
                                 points: [
-                                   [40.280455, 116.163251].reverse(),
-                                   [82.062459, -77.056620].reverse()
+                                    [40.280455, 116.163251].reverse(),
+                                    [82.062459, -77.056620].reverse()
                                 ],
                                 color: 'transparent',
                                 speed: 70,
                                 animation: 'normal',
-                                symbol:{
+                                symbol: {
                                     draw: 'fill',
                                     color: '#fff',
                                     scale: 0.02,
                                     rotate: -135,
-                                    speed: 170,
+                                    speed: 70,
                                     d: "M960 384H731.42L521.22 16.12A32.028 32.028 0 0 0 493.42 0h-131c-21.26 0-36.6 20.34-30.76 40.78L429.72 384H224l-86.4-115.2c-6.04-8.06-15.54-12.8-25.6-12.8H32.02C11.2 256-4.08 275.56 0.98 295.76L64 512 0.98 728.24C-4.08 748.44 11.2 768 32.02 768H112c10.08 0 19.56-4.74 25.6-12.8L224 640h205.72l-98.06 343.2c-5.84 20.44 9.5 40.8 30.76 40.8h131c11.48 0 22.08-6.16 27.78-16.12L731.42 640H960c70.7 0 192-57.3 192-128s-121.3-128-192-128z"
                                 }
                             }
@@ -100,7 +102,7 @@ function NotFound() {
             </div>
         </div>
         <div className={style['map']}>
-            <span className={style['map-title']}>you can use 'easy-drawing' to draw a local map</span>
+            <span className={style['map-title']}>{lang === 'en' ? "you can use 'easy-drawing' to draw a local map" : "你可以用easy-drawing画一幅本地地图"}</span>
             <div className={style['map-content']}>
                 <EasyDrawing
                     areas={
@@ -163,7 +165,7 @@ function NotFound() {
             </div>
         </div>
         <div ref={dom} className={style['chart']}>
-            <span className={style['map-title']}>you can use 'easy-drawing' to draw a chart</span>
+            <span className={style['map-title']}>{lang  === 'en' ? "you can use 'easy-drawing' to draw a chart" : "甚至你可以用easy-drawing画一个图表"}</span>
             <div className={style['map-content']}>
                 <Chart fontSize={size} />
             </div>
